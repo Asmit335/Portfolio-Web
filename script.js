30c345f82299ffef6af00310672d7bad7b4ae689
AsmitBro <asmitkhanal@gmail.com>
2022-12-29 11:25:51 +0545

    About section updated

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// navbar js
 $(document).ready(function() {
     $(window).scroll(function() {
         if (this.scrollY > 20) {
@@ -15,3 +16,19 @@ $(document).ready(function() {
 
 
 });
+
+// about section click js
+
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-links")
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-links");
+    document.getElementById(tabname).classList.add("active-tab")
+}
